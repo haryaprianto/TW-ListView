6f3a7c09c27d513f2e9f6aab46efecbde372997f
haryaprianto <16022596@myrp.edu.sg>
2018-04-24 12:37:50 +0800

    FirstActivityDone

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,9 +1,14 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,9 +23,32 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lv = (ListView) findViewById(R.id.lvYear);
 
         al = new ArrayList<String>();
         al.add("Year 1");
+        al.add("Year 2");
+        al.add("Year 3");
+
+        ArrayAdapter aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, al);
+        lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String year = al.get(position);
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("year", year);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
 
 
 
